65206fe64ac3cd7eef0f84f396535ca9455a02cf
Tashfia Rahman <trahman@lawn-128-61-6-223.lawn.gatech.edu>
2019-09-20 13:09:04 -0400

    page redirection implemented

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # spacetrader_brogrammers
 The Brogrammers code up a Space Traderz game in Python Flask!
 PS: I updated some stuff - Saurav :)
+
+Current progress: We have a home page that redirects to a character page, which will then redirect to the confirmation page
+
+To-do: 
+1. Skills points functionality on the character page
+2. Displaying everything from the character page on the confirmation page
+
--- application.py
+++ application.py
@@ -11,30 +11,30 @@ app.config['WTF_CSRF_ENABLED'] = False
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    fl_form = homePageForm()
-    if fl_form.is_submitted():
+    home_form = homePageForm()
+    if home_form.is_submitted():
         return redirect(url_for('character'))
     else: 
-        return render_template('home.html', html_form=fl_form)
+        return render_template('home.html', html_form=home_form)
     
 #try adding '/character' to the url! :)
 @app.route('/character', methods=['GET', 'POST'])
 def character():
-    fl_form = characterForm()
-    if fl_form.is_submitted():
-        return redirect(url_for('character'))
+    character_form = characterForm()
+    if character_form.is_submitted():
+        return redirect(url_for('confirmation'))
     else:
-        return render_template('character.html', html_form=fl_form)
+        return render_template('character.html', html_form=character_form)
 
 
 #try adding '/characterinfo' to the url! :)
-@app.route('/characterinfo', methods=['GET', 'POST'])
-def characterinfo():
-    fl_form = characterFormInfo()
-    if fl_form.is_submitted():
+@app.route('/confirmation', methods=['GET', 'POST'])
+def confirmation():
+    confirmation_form = confirmationForm()
+    if confirmation_form.is_submitted():
         return redirect(url_for('character'))
     else:
-        return render_template('character.html', html_form=fl_form)
+        return render_template('confirmation.html', html_form=confirmation_form)
 
 
 class homePageForm(FlaskForm):
@@ -44,7 +44,7 @@ class characterForm(FlaskForm):
     name_field = StringField('Input')
     submit = SubmitField("Submit")
 
-class characterFormInfo(FlaskForm):
+class confirmationForm(FlaskForm):
     name_field = StringField('Input')
     submit = SubmitField("Submit")
 
