dffaff8bcbfa9e0dbd756a14b7c67c2a38ec40bc
Saurav Ghosal <sghosal9@gatech.edu>
2019-10-02 19:53:35 -0400

    formatting

--- Game.py
+++ Game.py
@@ -3,8 +3,7 @@ from flask import Flask
 
 class Game:
     def __init__(self, difficulty):
-        self.universe = Universe(
-  ``          ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'])
+        self.universe = Universe(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'])
         self.difficulty = difficulty
 
     def startGame(self, player):
