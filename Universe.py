63be7de757b5c3df14d2fe32260c7f809cddbd9f
Tashfia Rahman <trahman@lawn-128-61-3-159.lawn.gatech.edu>
2019-10-02 20:03:59 -0400

    updated Universe

--- Universe.py
+++ Universe.py
@@ -1,5 +1,16 @@
+import TechLevel
 class Universe(): #params list of Regions
     #Universe is a list of regions
-    def __init__ (regionsList):
-        self.regionsList = regionsList
+    def __init__ (regionNames):
+        self.regionNames = regionNames
+        self.regions = []
+        self.coordsList = []
+        for i in regionNames:
+            coords = (5 * random.randint(-40, 40), 5 * random.randint(-40, 40))
+            while coords in coordsList:
+                coords = (5 * random.randint(-40, 40), 5 * random.randint(-40, 40))
+            techLevel = random.choice(list(TechLevel))
+            regions.append(Region(coords[0], coords[1], techLevel, regionNames[i])) 
+
+        
         
