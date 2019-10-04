963be84ae778b13b0a92a6c2beea111efae9d959
Saurav Ghosal <sghosal9@gatech.edu>
2019-10-04 17:43:31 -0400

    fixed up styling - adheres to pylint

--- application.py
+++ application.py
@@ -1,87 +1,81 @@
-# Check out the link to this Github for more info!
-# https://github.com/RachitBhargava99/Flask-Tutorials
+""" This file contains the page redirection and some of the back-end logic for our game! """
 
 from flask import Flask, render_template, request, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField, RadioField
-from Game import homePageForm, Game
 from flask_bootstrap import Bootstrap  #not needed anymore.. but may be good to keep for later!
+from Game import homePageForm, Game
 from Player import Player, playerForm
-from Universe import Universe, UniverseForm
+from Universe import UniverseForm
 
-app = Flask(__name__)
-Bootstrap(app)
-app.config['WTF_CSRF_ENABLED'] = False
+APP = Flask(__name__)
+Bootstrap(APP)
+APP.config['WTF_CSRF_ENABLED'] = False
 
 # Game object - everything is in this object
-game = Game()
+GAME = Game()
 
 
-@app.route('/', methods=['GET', 'POST'])
+@APP.route('/', methods=['GET', 'POST'])
 def home():
+    """ Displays home page with button to begin the Game """
+
     fl_form = homePageForm()
     if fl_form.validate_on_submit():
         return redirect(url_for('character'))
-    else:
-        return render_template('home.html', html_form=fl_form)
+    return render_template('home.html', html_form=fl_form)
 
 
-@app.route('/character', methods=['GET', 'POST'])
+@APP.route('/character', methods=['GET', 'POST'])
 def character():
+    """ Displays page for user to customize character and starts a new Game with provided inputs """
+
     fl_form = playerForm()
     if fl_form.validate_on_submit():
-        if (str(fl_form.name.data) != ""):
-            p1 = Player(fl_form.pilot.data, fl_form.merchant.data,
-                        fl_form.fighter.data, fl_form.engineer.data,
-                        int(fl_form.difficulty.data), fl_form.name.data)
-            if (fl_form.difficulty.data == "1000"):
-                p1.skill_level = 16
-                game.startGame(p1, 'Easy')
-            elif (fl_form.difficulty.data == "500"):
-                p1.skill_level = 12
-                game.startGame(p1, 'Medium')
+        if str(fl_form.name.data) != "":
+            player_1 = Player(fl_form.pilot.data, fl_form.merchant.data,
+                              fl_form.fighter.data, fl_form.engineer.data,
+                              int(fl_form.difficulty.data), fl_form.name.data)
+            if fl_form.difficulty.data == "1000":
+                player_1.skill_level = 16
+                GAME.startGame(player_1, 'Easy')
+            elif fl_form.difficulty.data == "500":
+                player_1.skill_level = 12
+                GAME.startGame(player_1, 'Medium')
             else:
-                p1.skill_level = 8
-                game.startGame(p1, 'Hard')
+                player_1.skill_level = 8
+                GAME.startGame(player_1, 'Hard')
 
-            if (game.player.checkPoints()):
+            if GAME.player.checkPoints():
                 return redirect(url_for('characterinfo'))
-            else:
-                return render_template(
-                    'character.html',
-                    html_form=fl_form,
-                    html_message=
-                    'Something is wrong with your submission. Please try again.'
-                )
-    else:
-        return render_template('character.html',
-                               html_form=fl_form,
-                               html_message='No errors detected')
+            return render_template(
+                'character.html',
+                html_form=fl_form,
+                html_message=
+                'Something is wrong with your submission. Please try again.')
+    return render_template('character.html',
+                           html_form=fl_form,
+                           html_message='No errors detected')
 
 
-# Redirection for region page still needs to be implemented
-
-
-@app.route('/characterinfo', methods=['GET', 'POST'])
+@APP.route('/characterinfo', methods=['GET', 'POST'])
 def characterinfo():
+    """ Displays character information with button to travel to another region """
+
     fl_form = UniverseForm()
     if fl_form.validate_on_submit():
         return redirect(url_for('regions'))
-    else:
-        return render_template('characterinfo.html',
-                               game=game,
-                               html_form=fl_form)
+    return render_template('characterinfo.html', game=GAME, html_form=fl_form)
 
 
-@app.route('/regions', methods=['GET', 'POST'])
+@APP.route('/regions', methods=['GET', 'POST'])
 def regions():
-    if request.method == 'POST' and request.form.get('regions') != None:
+    """ Displays a page containing all the regions """
+
+    if request.method == 'POST' and request.form.get('regions') is not None:
         new_region_index = request.form.get('regions')
-        game.curr_region = game.universe.find_region(int(new_region_index))
+        GAME.curr_region = GAME.universe.find_region(int(new_region_index))
         return redirect(url_for('characterinfo'))
-    else:
-        return render_template('regions.html', game=game)
+    return render_template('regions.html', game=GAME)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    APP.run(debug=True)
