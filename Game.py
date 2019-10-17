7858ae441769416827e66342b8fb92d2108a1fce
Saurav Ghosal <sghosal9@gatech.edu>
2019-10-17 01:35:27 -0400

    game chnages

--- Game.py
+++ Game.py
@@ -13,7 +13,6 @@ class Game:
 
     def start_game(self, player, difficulty):
         """Starts the game and instantiates all objects/attributes
-        Keyword arguments:F
         player -- the player that's added to the game
         difficulty -- the game's difficulty
         """
@@ -21,13 +20,34 @@ class Game:
         self.player = player
         self.universe = Universe(
             ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'], [])
-        self.curr_region = self.universe.pick_random_region() # this could be moved over to player class
-        
-        
+        self.curr_region = self.universe.pick_random_region()
+
     def travel(self, region):
-        distance = (self.curr_region.x - region.x)**2 + (self.curr_region.y - region.y)**2)**(1/2)
-        self.player.ship = 
-        self.curr_region = region
+        distance = (
+            (self.curr_region.x_coord - region.x_coord)**2 +
+            (self.curr_region.y_coord - region.y_coord)**2)**(1 / 2) / 10
+        fuel_cost = distance - self.player.pilot
+        if fuel_cost < 0:
+            fuel_cost = 0
+        if self.player.ship.current_fuel >= fuel_cost:
+            self.player.ship.current_fuel -= fuel_cost
+            self.curr_region = region
+            return True
+        else:
+            return False
+
+    def buy(self, item_key):
+        base_price = self.curr_region.market.get(item_key)
+        buying_price = base_price - self.player.merchant * 0.25
+        if buying_price <= 0:
+            buying_price = 0
+        if self.player.credits >= buying_price and len(
+                self.player.ship.inventory) < self.player.ship.cargo_space:
+            self.player.credits -= buying_price
+            self.player.ship.inventory.append(item_key)
+            return True
+        else:
+            return False
 
 
 class HomePageForm(FlaskForm):
