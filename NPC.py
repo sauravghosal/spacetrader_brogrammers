d6aaf25d378cb4469f890e13fdd3cf249f86d95a
Saurav Ghosal <sghosal9@gatech.edu>
2019-11-06 21:04:50 -0500

    npc page

---
+++ NPC.py
@@ -0,0 +1,14 @@
+import random
+
+
+class NPC:
+    def __init__(self):
+        pass
+
+
+class Bandit(NPC):
+    # demand
+    #
+    def __init__(self, difficulty):
+        super().__init__()
+        self.demand = random.randint(1, difficulty)
---
+++ Police.py
@@ -0,0 +1,6 @@
+import NPC
+
+
+class Police(NPC):
+    def __init__(self):
+        pass
