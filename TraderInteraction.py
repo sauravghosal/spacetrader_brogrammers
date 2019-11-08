5fbfb8c37757b6edea6a08bf62fb05a61b0fb54f
Saurav Ghosal <sghosal9@gatech.edu>
2019-11-08 12:21:30 -0500

    police, trader functionality

--- TraderInteraction.py
+++ TraderInteraction.py
@@ -1,7 +1,7 @@
 import random
 
 
-def TraderInteraction(game, option, negotiateOption):
+def TraderInteraction(game, option):
     traveled = True
     result = ""
     offeredItem = game.npc.itemKey
@@ -9,7 +9,6 @@ def TraderInteraction(game, option, negotiateOption):
     if option == 'Buy Items':
         game.buy(offeredItem)
         result = "You bought the trader's item!"
-    # elif option == 'Continue to Region':
     elif option == 'Rob Them >:)':
         if game.player.fighter >= random.randint(0, 16):
             game.player.ship.inventory.append(offeredItem)
@@ -23,16 +22,23 @@ def TraderInteraction(game, option, negotiateOption):
             game.player.credits += offeredCost / 2
             result = "You bought the item at a reduced price!"
         else:
-            if negotiateOption == 'Buy':
-                game.buy(offeredItem)
-                game.player.credits -= 2 * offeredCost
-                result = "You bought the item at an increased price!"
-            # elif negotiateOption == 'Ignore':
-            elif negotiateOption == "Rob":
-                if game.player.fighter >= random.randint(0, 16):
-                    game.player.ship.inventory.append(offeredItem)
-                    result = "You successfully robbed the trader!"
-                else:
-                    game.player.ship.health -= 10
-                    result = "Your ship took damage!"
+            result = "Not able to Negotiate"
     return [result, traveled]
+
+
+def Negotiate(game, negotiateOption):
+    offeredItem = game.npc.itemKey
+    offeredCost = game.npc.itemValue
+    if negotiateOption == 'Buy':
+        game.buy(offeredItem)
+        game.player.credits -= 2 * offeredCost
+        result = "You bought the item at an increased price!"
+    # elif negotiateOption == 'Ignore':
+    elif negotiateOption == "Rob":
+        if game.player.fighter >= random.randint(0, 16):
+            game.player.ship.inventory.append(offeredItem)
+            result = "You successfully robbed the trader!"
+        else:
+            game.player.ship.health -= 10
+            result = "Your ship took damage!"
+    return [result, True]
