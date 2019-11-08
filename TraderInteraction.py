0c1fba253d88c499540cce8263314f13377a44f4
Tashfia Rahman <trahman@lawn-128-61-5-246.lawn.gatech.edu>
2019-11-08 13:06:09 -0500

    clarified trader interaction

--- TraderInteraction.py
+++ TraderInteraction.py
@@ -7,15 +7,18 @@ def TraderInteraction(game, option):
     offeredItem = game.npc.itemKey
     offeredCost = game.npc.itemValue
     if option == 'Buy Items':
-        game.buy(offeredItem)
-        result = "You bought the trader's item!"
+        if game.player.credits >= offeredCost:
+            game.buy(offeredItem)
+            result = "You bought the trader's item!"
+        else:
+            result = "You don't have enough money to buy the item!"
     elif option == 'Rob Them >:)':
         if game.player.fighter >= random.randint(0, 16):
             game.player.ship.inventory.append(offeredItem)
             result = "You robbed the trader!"
         else:
             game.player.ship.health -= 10
-            result = "Your ship took damage!"
+            result = "You failed to rob the trader, so your ship took damage!"
     elif option == 'Negotiate':
         if game.player.merchant >= random.randint(0, 16):
             game.buy(offeredItem)
@@ -42,5 +45,5 @@ def Negotiate(game, negotiateOption):
             result = "You successfully robbed the trader!"
         else:
             game.player.ship.health -= 10
-            result = "Your ship took damage!"
+            result = "You failed to rob the trader, so your ship took damage!"
     return result
