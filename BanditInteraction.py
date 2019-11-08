dd18a5f2b497ff88417eb9d003f191be870dedfe
Tashfia Rahman <trahman@lawn-128-61-2-93.lawn.gatech.edu>
2019-11-08 01:39:41 -0500

    trader and police

--- BanditInteraction.py
+++ BanditInteraction.py
@@ -2,24 +2,28 @@ import random
 
 
 # when fleeing go back to travel page
-def BanditInteraction(game, option, region):
+def BanditInteraction(game, option):
     result = ""
+    traveled = True
     if option == 'Pay the Demand':
         if game.player.credits <= game.npc.demand:
             if len(game.player.ship.inventory):
                 game.loseAllItems()
-                result = "Lost all your items"
+                result = "You lost all your items"
             else:
                 game.player.ship.health -= game.npc.damage
-                result = "You took ship damage"
+                result = "You don't have enough credits to pay the demand, and your ship takes damage!"
         else:
             game.player.credit -= game.npc.demand
             result = "You lost credits!"
     elif option == 'Flee':
-        if (game.player.pilot >= random.randint(0, 16)):
-            game.loseAllMoney()
+        if (game.player.pilot <= random.randint(0, 16)):
+            game.loseAllCredits()
             game.player.ship.health -= game.npc.damage
             result = "You lost all your credits & took ship damage!"
+        else:
+            result = "You successfully flee."
+            traveled = False
     else:
         if game.player.fighter >= random.randint(0, 16):
             game.player.credits += 100
@@ -28,4 +32,4 @@ def BanditInteraction(game, option, region):
             game.loseAllCredits()
             game.player.ship.health -= game.npc.damage
             result = "You lost all your credits & took ship damage!"
-    return result
+    return [result, traveled]
--- PoliceInteraction.py
+++ PoliceInteraction.py
@@ -1,21 +1,25 @@
 import random
 
-def PoliceInteraction(game, option, region):
-    if result = ""
+def PoliceInteraction(game, option):
+    traveled = True
+    result = ""
     if option == 'Forfeit the Items':
         game.loseAllItems()
-        result = "Lost all your items"
+        result = "You lost all your items!"
     elif option == 'Flee':
         if (game.player.pilot <= random.randint(0, 16)):
             game.loseRandomItem()
             game.player.ship.health -= 10
             game.loseMoney(10)
-            game.curr_region = region
+            traveled = False
+            result = "You lost the item, damaged ship health, and lost credits!"
     elif option == 'Fight':
         if (game.player.fighter <= random.randint(0, 16)):
-            game.curr_region = region
+            result = "You successfully fought off the police!"
         else:
             game.loseAllItems()
             game.player.ship.health -= 10
             game.loseMoney(10)
-            game.curr_region= = region
+            traveled = False
+            result = "You lost all your items, your ship took damage, and you lost credits!"
+    return [result, traveled]
